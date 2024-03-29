92472055f3c44311f1230efefc0b00e954cf382c
vsened <vasily.senederov@gmail.com>
2024-03-29 22:37:53 +0500

    Task006Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        button.setOnClickListener {
+            textView.text = getString(R.string.i_am_an_android_developer)
+        }
+    }
+
+    private fun initViews() {
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.changeButton)
     }
 }
